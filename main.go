715eb075c835e70b4d021bd8fe543d7c6ccf7efd
jdc996 <jdcubillos96@gmail.com>
2019-05-04 23:11:00 -0500

    Se agrega el crud de los partidos para el api de liga sudamericana

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	_ "github.com/jdc996/partidos_sudamericana_crud/routers"
+
+	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/logs"
+	"github.com/astaxie/beego/orm"
+	"github.com/astaxie/beego/plugins/cors"
+	_ "github.com/lib/pq"
+	"github.com/udistrital/utils_oas/customerror"
+)
+
+func main() {
+	orm.RegisterDataBase("default", "postgres", beego.AppConfig.String("sqlconn"))
+	if beego.BConfig.RunMode == "dev" {
+		beego.BConfig.WebConfig.DirectoryIndex = true
+		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
+	}
+	beego.InsertFilter("*", beego.BeforeRouter, cors.Allow(&cors.Options{
+		AllowOrigins: []string{"*"},
+		AllowMethods: []string{"PUT", "PATCH", "GET", "POST", "OPTIONS", "DELETE"},
+		AllowHeaders: []string{"Origin", "x-requested-with",
+			"content-type",
+			"accept",
+			"origin",
+			"authorization",
+			"x-csrftoken"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+	}))
+	logs.SetLogger(logs.AdapterFile, `{"filename":"/var/log/beego/partidos_sudamericana_crud/partidos_sudamericana_crud.log"}`)
+	beego.ErrorController(&customerror.CustomErrorController{})
+	beego.Run()
+}
